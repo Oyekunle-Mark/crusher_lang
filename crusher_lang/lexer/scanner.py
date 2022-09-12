c88e57ffa54a48757321aaa964923bf1200559d8
omarkoloyede <omarkoloyede@yelp.com>
2022-09-12 20:07:06 +0100

    Added handling for basic one or two character tokens

--- crusher_lang/lexer/scanner.py
+++ crusher_lang/lexer/scanner.py
@@ -26,14 +26,74 @@ class Scanner:
     def scan(self):
         self.__load_file
 
+        while not self.__at_end_of_file:
+            self.start = self.current
+            self.__scan_line()
+
+        self.input_scanned = True
+        return self.tokens
+
+    def __scan_line(self):
+        currentChar = self.__advance_char()
+
+        if currentChar == "(":
+            self.__add_token(TokenType.LEFT_PAREN)
+        elif currentChar == ")":
+            self.__add_token(TokenType.RIGHT_PAREN)
+        elif currentChar == "{":
+            self.__add_token(TokenType.LEFT_BRACE)
+        elif currentChar == "}":
+            self.__add_token(TokenType.RIGHT_BRACE)
+        elif currentChar == ",":
+            self.__add_token(TokenType.COMMA)
+        elif currentChar == "-":
+            self.__add_token(TokenType.MINUS)
+        elif currentChar == "+":
+            self.__add_token(TokenType.PLUS)
+        elif currentChar == "/":
+            if self.__match_char("/"):
+                while self.__peek_char() != "\n" and not self.__at_end_of_file:
+                    self.__advance_char()
+
+            self.__add_token(TokenType.SLASH)
+        elif currentChar == "*":
+            self.__add_token(TokenType.STAR)
+        elif currentChar == "!":
+            token_type = (
+                TokenType.BANG_EQUAL if self.__match_char("=") else TokenType.BANG
+            )
+            self.__add_token(token_type)
+        elif currentChar == "=":
+            token_type = (
+                TokenType.EQUAL_EQUAL if self.__match_char("=") else TokenType.EQUAL
+            )
+            self.__add_token(token_type)
+        elif currentChar == ">":
+            token_type = (
+                TokenType.GREATER_EQUAL if self.__match_char("=") else TokenType.GREATER
+            )
+            self.__add_token(token_type)
+        elif currentChar == "<":
+            token_type = (
+                TokenType.LESS_EQUAL if self.__match_char("=") else TokenType.LESS
+            )
+            self.__add_token(token_type)
+        elif currentChar == ";":
+            self.__add_token(TokenType.SEMICOLON)
+        elif currentChar in [" ", "\r", "\t"]:
+            pass
+        elif currentChar == "\n":
+            self.line += 1
+        else:
+            pass  # raise exception here
+
     @property
     def __current_char(self):
         return self.raw_text[self.current]
 
-    @property
     def __peek_char(self):
-        if self.current == len(self.raw_text):
-            return TokenType.EOF
+        if self.__at_end_of_file:
+            return "\0"
 
         return self.raw_text[self.current + 1]
 
@@ -44,6 +104,9 @@ class Scanner:
         return previous
 
     def __match_char(self, char):
+        if self.__at_end_of_file:
+            return False
+
         if char != self.__current_char:
             return False
 
@@ -51,26 +114,17 @@ class Scanner:
         return True
 
     @property
-    def __at_end_of_tokens(self):
-        return self.input_scanned and self.current_token_index == len(self.tokens)
+    def __at_end_of_file(self):
+        return self.input_scanned and self.current == len(self.raw_text)
 
-    @property
-    def current_token(self):
-        return self.tokens[self.current_token_index]
-
-    @property
-    def peek(self):
-        return self.tokens[self.current_token_index + 1]
-
-    def advance(self):
-        previous = self.current_token
-        self.current_token_index += 1
-
-        return previous
-
-    def match(self, token):
-        if token != self.current_token:
-            return False
-
-        self.advance()
-        return True
+    def __add_token(self, token_type, literal=None):
+        lexeme = self.raw_text[self.start : self.self.current + 1]
+        self.tokens.append(
+            Token(
+                token_type=token_type,
+                lexeme=lexeme,
+                literal=literal,
+                line=self.line,
+                column=self.current,
+            )
+        )
