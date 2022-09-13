794a800c1ebfd76edb02933746c2f887d9998602
omarkoloyede <omarkoloyede@yelp.com>
2022-09-13 22:25:44 +0100

    Added statement nodes

--- crusher_lang/ast_generator/statement.py
+++ crusher_lang/ast_generator/statement.py
@@ -39,3 +39,97 @@ class Statement(ABC):
     @abstractmethod
     def accept(self, visitor):
         pass
+
+
+class ExpressionStatement(Statement):
+    def __init__(self, expr):
+        self.expr = expr
+
+    def accept(self, visitor):
+        return visitor.visit_expression(self)
+
+    def __str__(self):
+        return f"{self.expr}"
+
+
+class BlockStatement(Statement):
+    def __init__(self, statements):
+        self.statements = statements
+
+    def accept(self, visitor):
+        return visitor.visit_block(self)
+
+    def __str__(self):
+        return f"{[str(stmt) for stmt in self.statements]}"
+
+
+class FunctionStatement(Statement):
+    def __init__(self, name, parameters, body):
+        self.name = name
+        self.parameters = parameters
+        self.body = body
+
+    def accept(self, visitor):
+        return visitor.visit_function(self)
+
+    def __str__(self):
+        return f"fn {self.name.lexeme}({[str(param) for param in self.parameters]}) |{[str(stmt) for stmt in self.body]}|"
+
+
+class IfStatement(Statement):
+    def __init__(self, condition, if_branch, else_branch):
+        self.condition = condition
+        self.if_branch = if_branch
+        self.else_branch = else_branch
+
+    def accept(self, visitor):
+        return visitor.visit_if(self)
+
+    def __str__(self):
+        return f"if {self.condition} |{self.if_branch}| else |{self.else_branch}|"
+
+
+class PrintStatement(Statement):
+    def __init__(self, expr):
+        self.expr = expr
+
+    def accept(self, visitor):
+        return visitor.visit_print(self)
+
+    def __str__(self):
+        return f"print {self.expr}"
+
+
+class ReturnStatement(Statement):
+    def __init__(self, expr):
+        self.expr = expr
+
+    def accept(self, visitor):
+        return visitor.visit_return(self)
+
+    def __str__(self):
+        return f"return {self.expr}"
+
+
+class LetStatement(Statement):
+    def __init__(self, name, initializer):
+        self.name = name
+        self.initializer = initializer
+
+    def accept(self, visitor):
+        return visitor.visit_let(self)
+
+    def __str__(self):
+        return f"let {self.name.lexeme} = {self.initializer}"
+
+
+class WhileStatement(Statement):
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = body
+
+    def accept(self, visitor):
+        return visitor.f(self)
+
+    def __str__(self):
+        return f"while ({self.condition}) |{self.body}|"
